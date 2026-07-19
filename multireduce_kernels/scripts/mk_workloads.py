71e5e273ef399913ec6f668cfc1e79713d75033f
Andrew <andrewli200504@gmail.com>
2026-07-19 12:22:02 -0700

    Covariance

--- multireduce_kernels/scripts/mk_workloads.py
+++ multireduce_kernels/scripts/mk_workloads.py
@@ -6,9 +6,7 @@ def cosinesim(t0, t1):
 def l2norm(t0, t1):
     asq, ab, bsq = mk.asq_ab_bsq(t0, t1)
     return (asq - 2 * ab + bsq).sqrt()
-# def mk_cos_sim(asq, ab, bsq):
-#     return ab / (asq.sqrt() * bsq.sqrt())
-# def mk_l2_norm(asq, ab, bsq): 
-#     return (asq - 2 * ab + bsq).sqrt()
-# def mk_covariance(a, ab, b, n): 
-#     return (ab - 1 / n * a * b) / (n - 1)
+def covariance(t0, t1): 
+    n = t0.numel()
+    a, ab, b = mk.a_ab_b(t0, t1)
+    return (ab - 1 / n * a * b) / (n - 1)
--- multireduce_kernels/scripts/torch_helpers.py
+++ multireduce_kernels/scripts/torch_helpers.py
@@ -4,17 +4,20 @@ def sumsq(t):
 def summul(t0, t1):
     return (t0 * t1).sum()
 def cosinesim(t0, t1): 
-    #return F.cosine_similarity(t0, t1, dim=0)
-    #Fastest version of cosine_similarity on pytorch
     return torch.dot(t0, t1) / (torch.linalg.vector_norm(t0) * torch.linalg.vector_norm(t1))
-
 def l2norm(t0, t1):
     return torch.linalg.vector_norm(t0 - t1)
+def covariance(t0, t1): 
+    t0_centered = t0 - t0.mean()
+    t1_centered = t1 - t1.mean()
+    n = t0.numel()
+    return torch.dot(t0_centered, t1_centered) / (n - 1)
 SPECIAL_REDS = {
     "sumsq": sumsq,
     "asq": sumsq,
     "ab": summul,
     "bsq": sumsq,
     "cosinesim": cosinesim,
-    "l2norm": l2norm
+    "l2norm": l2norm,
+    "covariance": covariance
 }
